751cb0647294b17b24688abd4531af0a3c73b2b6
BrownC98 <0429donggun@gmail.com>
2025-02-16 05:19:07 +0900

    우주 쓰레기 이동속도 조절가능, y 좌표 랜덤화

--- Script/GameObjects/trash.js
+++ Script/GameObjects/trash.js
@@ -4,11 +4,24 @@ import { GameObject } from "../StructureCode/GameSystem.js";
 // 화면 왼쪽 끝에서 생성되고 오른쪽 끝으로 이동한다음 사라진다.
 export class trash extends GameObject {
 
+    constructor(speed) {
+        super();
+
+        // 우주쓰레기 이동속도
+        this.speed = speed || 1;
+    }
+
     Start() {
         console.log("우주 쓰레기 초기화");
         this.resource.image.src = "../../Resources/trash_1.png";
         this.transform.position.x = 0;
-        this.transform.position.y = 500; // TODO: 화면 맨 위에서 아래까지 랜덤한 y좌표에서 생성 되도록 개선하기
+
+        // 위 아래 패딩값 50 부여한 범위에서 랜덤한 y 좌표 생성
+        const padding = 50;
+        const min = padding;
+        const max = window.innerHeight - padding;
+        this.pointY = Math.random() * (max - min) + min;
+        this.transform.position.y = pointY;
         this.transform.scale.x = 1;
         this.transform.scale.y = 1;
         this.transform.anchor.x = 1;
@@ -18,10 +31,10 @@ export class trash extends GameObject {
 
     Update() {
         console.log(this.transform.position.x, this.transform.position.y);
-        
+
         // 매 프레임마다 오른쪽으로 1px씩 이동
         this.transform.position.x += 1;
-        
+
         // 화면 오른쪽 끝에 도달하면 오브젝트 삭제
         console.log(canvas.width);
         if (this.transform.position.x > canvas.width) {
