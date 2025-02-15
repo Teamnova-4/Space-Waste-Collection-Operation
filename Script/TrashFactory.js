a06b6925728b1d1df9900092e7c2d9df72f15e70
BrownC98 <0429donggun@gmail.com>
2025-02-16 06:06:21 +0900

    불필요한 코드 제거

--- Script/TrashFactory.js
+++ Script/TrashFactory.js
@@ -12,13 +12,11 @@ export class TrashFactory {
    
     // 우주 쓰레기 생성
      createTrash() {
-        if (this.isRunning) return;
 
         // 이동속도와 초당 생성 개수 반영
         setInterval(() => {
             const newTrash = new trash(this.speed);
         }, 1000 / this.spawnRate * 1000);
-        this.isRunning = true;
     }
 
     // 이동속도 설정
