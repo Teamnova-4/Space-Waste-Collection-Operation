b5e8cc69b39689c538b5fc510b270deaadc1d7b3
BrownC98 <0429donggun@gmail.com>
2025-02-20 16:54:56 +0900

    드론 판매 구현, 드론 슬롯 추가 가격표시

--- Script/Upgrade.js
+++ Script/Upgrade.js
@@ -48,7 +48,7 @@ export class User {
 
     static UseCredit(value){
         if (User.Instance().credits - value >= 0){
-            User.Instance().credits -= value;
+            User.Instance().setCredits(User.Instance().credits - value);
             return true;
         }
         AlertSystem.AddAlert("크레딧 부족", "크레딧이 부족합니다.");
